ea64fb8c5dc697bd5e37c0df68e88c25ba9bee56
Luiz Gustavo <sivaluiz9@gmail.com>
2024-09-05 09:59:13 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/luiz/wsmongodb/WsmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.wsmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
